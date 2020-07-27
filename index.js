8ef1b8ee1040199ee06bd5f3aa6b8883f8d91c55
diazfabio18 <fabiodiaz@est.frba.utn.edu.ar>
2020-07-27 19:53:17 -0300

    get latest photos from instagram

--- index.js
+++ index.js
@@ -2,10 +2,39 @@
 
 const fs = require('fs').promises;
 
+const fetch = require('node-fetch');
+
 //const iconsPlaceholder = '%{{icon_size}}%';
 
-fs.readFile('./Readme.md.tpl', {encoding: 'utf-8'})
+/*fs.readFile('./Readme.md.tpl', {encoding: 'utf-8'})
 .then(markdownTemplate =>{
     console.log(markdownTemplate);
-})
+})*/
 
+const getPhotosFromInstagram = () =>
+  fetch('https://instagram.com/fl_diaz?__a=1')
+    .then(res => res.json())
+    .then(({graphql}) => {
+      const { user } = graphql
+      const {edge_owner_to_timeline_media: {edges}} = user
+      return edges
+    })
+
+    ;(async () => {
+        const [template, photos] = await Promise.all([
+          fs.readFile('README.md.tpl', { encoding: 'utf-8' }),
+          getPhotosFromInstagram()
+        ])
+      
+        // create latest photos from instagram
+        const latestInstagramPhotos = photos
+          .slice(0, 5)
+          //.map(({node}) => generateInstagramHTML(node))
+          //.join('')
+
+        latestInstagramPhotos.map(({node}) =>
+            console.log(node.display_url)    
+        )
+
+
+    })()
